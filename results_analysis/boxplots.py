7c3f1d1d4b8c7a20d197057e3866f744b59e0fbd
dan.gn <daanieelgn@gmail.com>
2022-12-02 18:04:53 -0600

    Update boxplots.py

--- results_analysis/boxplots.py
+++ results_analysis/boxplots.py
@@ -4,13 +4,15 @@ import sys
 import csv
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+import seaborn
 
 current = os.path.dirname(os.path.realpath(__file__))
 parent = os.path.dirname(current)
 sys.path.append(parent)
 
-from experiment_info import N_EXPERIMENTS
-from experiment_info import datasets
+from experiment_info import N_EXPERIMENTS, datasets, experiment
 
 def csv2expdata(filename):
     metric = {}
@@ -20,7 +22,15 @@ def csv2expdata(filename):
             metric[row[0]] = row[1:]
     return metric
 
-filename = 'final_exp/sms_moneat/results_sms_moneat_final6_full_loss_arch.csv'
-data = pd.read_csv(filename)
+alg = 'n3o'
+exp = experiment[alg]
+filename = f'final_exp/{alg}/results_{alg}_final{exp}_full_loss_arch.csv'
+df = pd.read_csv(filename, header=0, index_col=0).transpose()
 
-print(data.shape)
+fig = px.box(df[datasets[0]])
+fig.show()
+
+# seaborn.set(style='whitegrid')
+# seaborn.boxplot(data=df)
+# plt.xticks(rotation='vertical')
+# plt.show()
